09047441c09d834e4bf50e164562c4d2327339d6
bianca-pokhrel <bianca.pokhrel@mail.utoronto.ca>
2020-11-02 12:41:25 -0500

    finally done predictions, just need graphs

--- part3/model_fit.R
+++ part3/model_fit.R
@@ -32,69 +32,13 @@ library(dplyr)
 library(ggplot2)
 library(readxl)
 library(foreign)
-library(arm)
-library(brms)
 library(effects)
 library(haven)
-library(aod)
-#### create some simulated data ####
+
 load("Part1/cleaned_survey_data.Rdata")
+#source("Part\ 2/01-data_cleaning-post-strat.R")
 size <- 6479
 
-
-sim_data <- tibble(
-  gender = sample(c('Male', 'Female'), size, replace = TRUE, prob = c(0.51, 0.49)),
-  interest = sample(c("Most of the time", "Some of the time", "Only now and then", "Hardly at all", NA), 
-                    size, replace = TRUE, prob = c(0.447, 0.354, 0.141, 0.057, 0.001)),
-  registration = sample(c("Registered", "Not registered", "Don't know", NA),
-                        size, replace = TRUE, prob = c(0.826, 0.149, 0.023, 0.002)),
-  vote_2016 = sample(c('Donald Trump', 'Hillary Clinton', "Did not vote, but was eligible",
-                       "Was not eligible to vote", "Gary Johnson", "Don't recall",
-                       "Other"), size, replace = TRUE, prob = c(0.337, 0.312, 0.153, 0.114, 0.027, 0.024, 0.033)),
-  vote_intention = sample(c("Yes, I will vote", "No I will not vote but I am eligible",
-                            "No, I am not eligible to vote", "Not sure", NA), size,
-                          replace = TRUE, prob = c(0.798, 0.072, 0.045, 0.082, 0.003)),
-  vote_2020 = sample(c("Donald Trump", "Joe Biden", "Someone else", "I would not vote",
-                       "I am not sure/don't know", NA), size, replace = TRUE,
-                     prob = c(0.383, 0.419, 0.038, 0.057, 0.1, 0.003)),
-  trump_biden = sample(c("Biden", NA), size, replace = TRUE, prob = c(0.4746, 0.525)),
-  ideo5 = sample(c("Very Liberal", "Liberal", "Moderate", "Conservative",
-                   "Very Conservative", "Not Sure", NA), size, replace = TRUE,
-                 prob = c(0.107, 0.165, 0.339, 0.177, 0.109, 0.098, 0.005)),
-  employment = sample(c("Full-time employed", "Retired", "Unemployed or temporarily on layoff",
-                        "Part-time employed", "Self-employed", "Other", NA),
-                      size, replace = TRUE, prob = c(0.392, 0.172, 0.106, 0.091, 0.064, 0.172, 0.003)),
-  foreign_born = sample(c("The United States", "Another country"), size, replace = TRUE,
-                        prob = c(0.928, 0.072)),
-  census_region = sample(c("Northeast", "Midwest", "South", "West"), size,
-                         replace = TRUE, prob = c(0.193, 0.208, 0.375, 0.224)),
-  hispanic = sample(c("Not Hispanic", "Hispanic"), size, replace = TRUE,
-                    prob = c(0.848, 0.152)),
-  race_ethnicity = sample(c("White", "Black, or African American", "Some other race",
-                            "Asian (Asian Indian)", "American Indian or Alaska Native",
-                            "Asian (Chinese)", "Other"), size, replace = TRUE,
-                          prob = c(0.743, 0.119, 0.071, 0.015, 0.014, 0.013, 0.025)),
-  household_income = sample(c("Less than $14,999", "$100,000 to $14,999", 
-                              "$25,000 to $29,999", "$30,000, $34,999", 
-                              "$50,000 to $54,999", "Other", NA), size, replace = TRUE,
-                            prob = c(0.150, 0.071, 0.052, 0.051, 0.051, 0.567, 0.058)),
-  education = sample(c("College Degree (such as B.A, B.S.)", "Completed some college, but no degree",
-                       "High school graduate", "Masters degree", "Completed some high school",
-                       "Associate Degree", "Other"), size, replace = TRUE,
-                     prob = c(0.228, 0.205, 0.166, 0.099, 0.098, 0.0879, 0.1149)))
-
-#make the trump_biden variable binary... replace NA with 1 for Trump, replace Biden with 0
-sim_data$trump_biden[sim_data$trump_biden %in% NA] <- 1
-sim_data$trump_biden[sim_data$trump_biden %in% "Biden"] <- 0
-
-#convert from chr to numeric
-sim_data$trump_biden = as.numeric(sim_data$trump_biden)
-
-# check survey
-head(sim_data)
-
-#check summary statistics
-summary(sim_data)
 
 ### ok now I have the real data ###
 
@@ -112,14 +56,14 @@ set.seed(123)
 library(MASS)
 
 #run an OLS regression to start
-OLS_model <- lm(trump_biden ~ gender + state + race_ethnicity + 
-                  household_income + age, 
+OLS_model <- lm(trump_biden ~ gender + factor(census_region) + factor(race_ethnicity) + 
+                  factor(household_income) + factor(age_group), 
                 data = cleaned_data)
 broom::tidy(OLS_model)
 
 #plot just to see what's up... give analysis for why this definitely isn't a good model
-layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page
-plot(OLS_model)
+# layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page
+# plot(OLS_model)
 
 #mkay so definitely not good model... QQ plot is def not linear & residuals vs. fitted aren't random/independent
 #clearly there's something else going on... spoiler- looks like it's logistic regression
@@ -133,8 +77,6 @@ summary(log_fit_full)
 
 # ok see what's up
 broom::tidy(log_fit_full)
-layout(matrix(c(1,2,3,4),2,2))
-plot(log_fit_full, main = "FULL")
 
 #much better!
 # now to see which variables we can drop- combo of anova + AIC
@@ -236,10 +178,70 @@ age_prop <- cleaned_data_strat_count %>%
 
 # predictions based on each variable
 
+# age
+age_pred <- data.frame(predict(log_fit_full, newdata = age_prop, type = "response"))
+colnames(age_pred) = "vote_pred"
 
+age_prop <- cbind(age_prop, age_pred)
+age_prop %>%
+  mutate (age_predict_prop = vote_pred*prop) %>%
+  group_by(age_group) %>%
+  summarise(vote_pred = sum(age_predict_prop)) %>%
+  summarise(mean = mean(vote_pred),
+            lower = quantile(vote_pred, 0.025),
+            higher = quantile(vote_pred, 0.975))
 
+# gender
+gender_pred <- data.frame(predict(log_fit_full, newdata = gender_prop, type = "response"))
+colnames(gender_pred) = "vote_pred"
 
+gender_prop <- cbind(gender_prop, gender_pred)
+gender_prop %>%
+  mutate (gender_predict_prop = vote_pred*prop) %>%
+  group_by(gender) %>%
+  summarise(vote_pred = sum(gender_predict_prop)) %>%
+  summarise(mean = mean(vote_pred),
+            lower = quantile(vote_pred, 0.025),
+            higher = quantile(vote_pred, 0.975))
 
+# race
+race_pred <- data.frame(predict(log_fit_full, newdata = race_prop, type = "response"))
+colnames(race_pred) = "vote_pred"
+
+race_prop <- cbind(race_prop, race_pred)
+race_prop %>%
+  mutate (race_predict_prop = vote_pred*prop) %>%
+  group_by(race_ethnicity) %>%
+  summarise(vote_pred = sum(race_predict_prop)) %>%
+  summarise(mean = mean(vote_pred),
+            lower = quantile(vote_pred, 0.025),
+            higher = quantile(vote_pred, 0.975))
+
+# region
+region_pred <- data.frame(predict(log_fit_full, newdata = region_prop, type = "response"))
+colnames(region_pred) = "vote_pred"
+
+region_prop <- cbind(region_prop, region_pred)
+region_prop %>%
+  mutate (region_predict_prop = vote_pred*prop) %>%
+  group_by(census_region) %>%
+  summarise(vote_pred = sum(region_predict_prop)) %>%
+  summarise(mean = mean(vote_pred),
+            lower = quantile(vote_pred, 0.025),
+            higher = quantile(vote_pred, 0.975))
+
+# income
+income_pred <- data.frame(predict(log_fit_full, newdata = income_prop, type = "response"))
+colnames(income_pred) = "vote_pred"
+
+income_prop <- cbind(income_prop, income_pred)
+income_prop %>%
+  mutate (income_predict_prop = vote_pred*prop) %>%
+  group_by(household_income) %>%
+  summarise(vote_pred = sum(income_predict_prop)) %>%
+  summarise(mean = mean(vote_pred),
+            lower = quantile(vote_pred, 0.025),
+            higher = quantile(vote_pred, 0.975))
 
 
 ### brm ###
